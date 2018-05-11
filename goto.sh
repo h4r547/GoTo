859212e9daab3a2e1d47e6e90a1f4de8d1763f66
PrateekPisat <pisatprateek12@gmail.com>
2018-05-11 13:15:25 -0400

    Ignore the Tutorial.

--- goto.sh
+++ goto.sh
@@ -4,12 +4,32 @@ if [ "$1" = "-s" ]
 then
   echo "Bookmark Name: "
   read bm
-  printf "$bm : $(pwd)" >> ~/GoTo/goto.conf
-  echo "Save"
+  if [ $(grep -c "$bm " ~/GoTo/goto.conf)  = 0 ]
+  then
+    printf "$bm : $(pwd)" >> ~/GoTo/goto.conf
+    echo "Save"
+  else
+    echo "Bookmark Already Exists"
+  fi
 elif [ "$1" = "-g" ]
 then
   echo "Where to go? "
   read bm
-  loc=$(grep -w "$bm" ~/GoTo/goto.conf | cut -d ':' -f 2)
-  cd $loc
+  if [ $(grep -c -i "$bm " ~/GoTo/goto.conf)  = 0 ]
+  then
+    echo "Bookmark doesn't exist"
+  else
+    loc=$(grep -i "$bm " ~/GoTo/goto.conf | cut -d ':' -f 2)
+    if [ -d $loc ]
+    then
+      cd $loc
+    else
+      echo "The directory does not exist."
+      grep -v "$loc" ~/GoTo/goto.conf > temp.txt
+      mv temp.txt ~/GoTo/goto.conf
+      echo "The entry has been deleted."
+    fi
+  fi
+else
+  echo "Invalid Option. Use '-s' to save this location. Use '-g' to goto a location"
 fi
